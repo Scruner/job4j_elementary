b153fc0c0f3cdad2ae9d0c1a91a4bcccc7a35ea8
Scruner <aoveden1977@mail.ru>
2021-04-09 18:38:38 +0300

    9.1. String [#515557]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+//Variant_1
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + " " + "But I am a newbie.";
+        int year = 2021;
+        idea = idea + " " + year;
+        System.out.println(idea);
+    }
+}
+
+//Variant_2
+//public class Greeting {
+//    public static void main(String[] args) {
+//        String idea = "I like Java!";
+//        System.out.println(idea);
+//        idea = idea + " But I am a newbie. ";
+//        int year = 2021;
+//        idea = idea + year;
+//        System.out.println(idea);
+//    }
+//}
