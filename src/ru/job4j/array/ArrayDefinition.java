a367654e344574c23afafafe732d1d0685a14c80
Scruner <aoveden1977@mail.ru>
2021-04-11 21:47:26 +0300

    6.0.2. Заполнение массива. [#515528]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Ivanov";
+        names[1] = "Ivan Petrov";
+        names[2] = "Semen Sidorov";
+        names[3] = "Ira Ivanova";
 
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surname.length);
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
