cf4219f2cbf7365a5e5eb8517e43d575b41e80fa
Scruner <aoveden1977@mail.ru>
2021-04-12 21:53:29 +0300

    6.2.3. Перевернуть массив. [#515531] delete main

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -10,9 +10,4 @@ public class Turn {
 
         return array;
     }
-
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        System.out.println(back(array));
-    }
 }
