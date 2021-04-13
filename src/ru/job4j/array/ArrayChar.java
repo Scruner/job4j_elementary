e9ca846b90cbe6ef4fd2781d069094fc5d754678
Scruner <aoveden1977@mail.ru>
2021-04-13 11:09:20 +0300

    6.4. Слово начинается с ... [#515516]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        int shortestLength;
+        if (word.length <= pref.length) {
+            shortestLength = word.length;
+        } else {
+            shortestLength = pref.length;
+        }
+        for (int i = 0; i < shortestLength; i++) {
+            if (word[i] != pref[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
