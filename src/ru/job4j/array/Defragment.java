a3f7500a48996bc5f0ba3ba763bd4756afef1ce9
Scruner <aoveden1977@mail.ru>
2021-04-15 13:21:15 +0300

    6.8. Дефрагментация массива [#515542] index + 1

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -5,7 +5,7 @@ public class Defragment {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
                 int point = index; /* указатель на null ячейку. */
-                for (int i = index; i < array.length; i++) {
+                for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
                         array[point] = array[i];
                         array[i] = null;
