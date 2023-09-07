eff8182103f54c2ee38ec06b9621fd67e86afbab
scruner <scruner@scruner.ru>
2023-09-07 10:00:18 +0300

    StringRunner

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,14 @@
+//package ru.job4j.array;
+//
+//public class SimpleStringEncoder {
+//
+//  public static String encode(String input) {
+//    String result = "aaabbbvdddrrr";
+//    char symbol = input.charAt(0);
+//    int counter = 1;
+//    for (int i = 0; i < result.length(); i++) {
+//      if ()
+//    }
+//    return result;
+//  }
+//}
